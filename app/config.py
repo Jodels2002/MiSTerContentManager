d6ef6d5d0be044efa355cc91ad101a9a50ee0c08
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2026-04-28 18:03:21 +0200

    Add configuration for ROM extensions and paths

---
+++ app/config.py
@@ -0,0 +1,6 @@
+ROM_EXTENSIONS = [".nes", ".sfc", ".smc", ".bin", ".gen"]
+
+ROMS_PATH = "/media/fat/games"   # anpassen!
+DB_PATH = "data/db.sqlite"
+DAT_PATH = "data/dats"
+MGL_PATH = "output/mgl"
